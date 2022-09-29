c4317a12775ed259f765b4bfa1655a5e96f9115c
Kristina Andersen <kris91r3@stud.kea.dk>
2022-09-29 20:02:46 +0200

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("daisyui"),
+  ],
+
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+};
